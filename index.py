1b9c0044ebef3ec637e4b8047536ebf4fb5d8dab
ksusonic <dan20082001@mail.ru>
2020-01-03 01:45:09 +0300

    Version 2

--- index.py
+++ index.py
@@ -1,9 +1,22 @@
-# Start amount of supplies: (Начальные запасы:)
-water = 1200
-milk = 540
-coffee = 120
-cups = 9
-money = 550
+# Initial supplies: (Начальные запасы:)
+water = 400  # ml of water
+milk = 540  # ml of milk
+coffee = 120  # g of coffee beans
+cups = 9  # disposable cups
+money = 550  # $ in cash
+
+# Recipes (supply_ ...  1 - espresso, 2 - latte, 3 - cappuccino
+water_1 = 250
+coffee_1 = 16
+money_1 = 4
+water_2 = 350
+milk_2 = 75
+coffee_2 = 20
+money_2 = 7
+water_3 = 200
+milk_3 = 100
+coffee_3 = 12
+money_3 = 6
 
 
 def status():
@@ -16,27 +29,58 @@ def status():
 
 
 def buy_action():
-    act = int(input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: '))
+    act = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ')
     global water
+    global coffee
     global milk
     global coffee
-    global cups
     global money
-    if act == 1:
-        water -= 250
-        coffee -= 16
-        money += 4
-    elif act == 2:
-        water -= 350
-        milk -= 75
-        coffee -= 20
-        money += 7
-    elif act == 3:
-        water -= 200
-        milk -= 100
-        coffee -= 12
-        money += 6
-    cups -= 1
+    global cups
+
+    if act == 'back':
+        return 0
+    if cups >= 1:
+        if act == '1':
+            if water < water_1:
+                print('Sorry, not enough water!')
+            elif coffee < coffee_1:
+                print('Sorry, not enough coffee beans!')
+            else:
+                print('I have enough resources, making you a coffee!')
+                water -= water_1
+                coffee -= coffee_1
+                cups -= 1
+                money += money_1
+        elif act == '2':
+            if water < water_2:
+                print('Sorry, not enough water!')
+            elif milk < milk_2:
+                print('Sorry, not enough milk!')
+            elif coffee < coffee_2:
+                print('Sorry, not enough coffee beans!')
+            else:
+                print('I have enough resources, making you a coffee!')
+                water -= water_2
+                milk -= milk_2
+                coffee -= coffee_2
+                cups -= 1
+                money += money_2
+        elif act == '3':
+            if water < water_3:
+                print('Sorry, not enough water!')
+            elif milk < milk_3:
+                print('Sorry, not enough milk!')
+            elif coffee < coffee_3:
+                print('Sorry, not enough coffee beans!')
+            else:
+                print('I have enough resources, making you a coffee!')
+                water -= water_3
+                milk -= milk_3
+                coffee -= coffee_3
+                cups -= 1
+                money += money_3
+    else:
+        print('Sorry, not enough disposable cups!')
 
 
 def fill_action():
@@ -56,14 +100,15 @@ def take_action():
     money = 0
 
 
+action = ''
 # Beginning the program:
-
-status()
-action = input('Write action (buy, fill, take): ')
-if action == 'buy':
-    buy_action()
-elif action == 'fill':
-    fill_action()
-elif action == 'take':
-    take_action()
-status()
+while action != 'exit':
+    action = input('Write action (buy, fill, take, remaining, exit): ')
+    if action == 'buy':
+        buy_action()
+    elif action == 'fill':
+        fill_action()
+    elif action == 'take':
+        take_action()
+    elif action == 'remaining':
+        status()
