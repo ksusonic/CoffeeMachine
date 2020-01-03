56b7971733f201473b82b8dc5ab27a592a73660b
ksusonic <dan20082001@mail.ru>
2020-01-03 17:44:39 +0300

    Продвинутая ООП - версия кофемашины

--- index.py
+++ index.py
@@ -1,114 +1,97 @@
-# Initial supplies: (Начальные запасы:)
-water = 400  # ml of water
-milk = 540  # ml of milk
-coffee = 120  # g of coffee beans
-cups = 9  # disposable cups
-money = 550  # $ in cash
+# Вариант ООП с поддержкой нескольких обьектов (машин)
+class CoffeeMachine:
 
-# Recipes (supply_ ...  1 - espresso, 2 - latte, 3 - cappuccino
-water_1 = 250
-coffee_1 = 16
-money_1 = 4
-water_2 = 350
-milk_2 = 75
-coffee_2 = 20
-money_2 = 7
-water_3 = 200
-milk_3 = 100
-coffee_3 = 12
-money_3 = 6
+    def __init__(self):
+        self.water = 400  # ml of water
+        self.milk = 540  # ml of milk
+        self.coffee = 120  # g of coffee beans
+        self.cups = 9  # disposable cups
+        self.money = 550  # $ in cash
 
+    def take(self):
+        job = input('Write action (buy, fill, take, remaining, exit): ')
+        if job == 'buy':
+            act = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ')
+            self.buy_action(act)
+        elif job == 'fill':
+            self.fill_action()
+        elif job == 'take':
+            self.take_action()
+        elif job == 'remaining':
+            self.__repr__()
+        elif job == 'exit':
+            global tag
+            tag = False
 
-def status():
-    print('The coffee machine has:')
-    print(water, 'of water')
-    print(milk, 'of milk')
-    print(coffee, 'of coffee beans')
-    print(cups, 'of disposable cups')
-    print(money, 'of money')
+    def __repr__(self):
+        print('The coffee machine has:')
+        print(self.water, 'of water')
+        print(self.milk, 'of milk')
+        print(self.coffee, 'of coffee beans')
+        print(self.cups, 'of disposable cups')
+        print(self.money, 'of money')
 
+    def buy_action(self, act):
 
-def buy_action():
-    act = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ')
-    global water
-    global coffee
-    global milk
-    global coffee
-    global money
-    global cups
+        if act == 'back':
+            self.take()
+        if self.cups >= 1:
+            if act == '1':
+                if self.water < 250:
+                    print('Sorry, not enough water!')
+                elif self.coffee < 16:
+                    print('Sorry, not enough coffee beans!')
+                else:
+                    print('I have enough resources, making you a coffee!')
+                    self.water -= 250
+                    self.coffee -= 16
+                    self.cups -= 1
+                    self.money += 4
+            elif act == '2':
+                if self.water < 350:
+                    print('Sorry, not enough water!')
+                elif self.milk < 75:
+                    print('Sorry, not enough milk!')
+                elif self.coffee < 20:
+                    print('Sorry, not enough coffee beans!')
+                else:
+                    print('I have enough resources, making you a coffee!')
+                    self.water -= 350
+                    self.milk -= 75
+                    self.coffee -= 20
+                    self.cups -= 1
+                    self.money += 7
+            elif act == '3':
+                if self.water < 200:
+                    print('Sorry, not enough water!')
+                elif self.milk < 100:
+                    print('Sorry, not enough milk!')
+                elif self.coffee < 12:
+                    print('Sorry, not enough coffee beans!')
+                else:
+                    print('I have enough resources, making you a coffee!')
+                    self.water -= 200
+                    self.milk -= 100
+                    self.coffee -= 12
+                    self.cups -= 1
+                    self.money += 6
+        else:
+            print('Sorry, not enough disposable cups!')
 
-    if act == 'back':
-        return 0
-    if cups >= 1:
-        if act == '1':
-            if water < water_1:
-                print('Sorry, not enough water!')
-            elif coffee < coffee_1:
-                print('Sorry, not enough coffee beans!')
-            else:
-                print('I have enough resources, making you a coffee!')
-                water -= water_1
-                coffee -= coffee_1
-                cups -= 1
-                money += money_1
-        elif act == '2':
-            if water < water_2:
-                print('Sorry, not enough water!')
-            elif milk < milk_2:
-                print('Sorry, not enough milk!')
-            elif coffee < coffee_2:
-                print('Sorry, not enough coffee beans!')
-            else:
-                print('I have enough resources, making you a coffee!')
-                water -= water_2
-                milk -= milk_2
-                coffee -= coffee_2
-                cups -= 1
-                money += money_2
-        elif act == '3':
-            if water < water_3:
-                print('Sorry, not enough water!')
-            elif milk < milk_3:
-                print('Sorry, not enough milk!')
-            elif coffee < coffee_3:
-                print('Sorry, not enough coffee beans!')
-            else:
-                print('I have enough resources, making you a coffee!')
-                water -= water_3
-                milk -= milk_3
-                coffee -= coffee_3
-                cups -= 1
-                money += money_3
-    else:
-        print('Sorry, not enough disposable cups!')
+    def fill_action(self):
+        self.water += int(input('Write how many ml of water do you want to add: '))
+        self.milk += int(input('Write how many ml of milk do you want to add: '))
+        self.coffee += int(input('Write how many grams of coffee beans do you want to add: '))
+        self.cups += int(input('Write how many disposable cups of coffee do you want to add: '))
 
-
-def fill_action():
-    global water
-    global milk
-    global coffee
-    global cups
-    water += int(input('Write how many ml of water do you want to add: '))
-    milk += int(input('Write how many ml of milk do you want to add: '))
-    coffee += int(input('Write how many grams of coffee beans do you want to add: '))
-    cups += int(input('Write how many disposable cups of coffee do you want to add: '))
+    def take_action(self):
+        print('I gave you', self.money)
+        self.money = 0
 
 
-def take_action():
-    global money
-    print('I gave you', money)
-    money = 0
+# Main
+machine1 = CoffeeMachine()
+tag = True
 
-
-action = ''
-# Beginning the program:
-while action != 'exit':
-    action = input('Write action (buy, fill, take, remaining, exit): ')
-    if action == 'buy':
-        buy_action()
-    elif action == 'fill':
-        fill_action()
-    elif action == 'take':
-        take_action()
-    elif action == 'remaining':
-        status()
+while tag:
+    machine1.take()
